83f75c88554ae1f0b166a4f3e0523814ba8c8376
joh <j.odagiri.2011@gmail.com>
2022-02-16 11:45:09 +0900

    管理画面(admin)にモデル(Profile)追加

--- app/models.py
+++ app/models.py
@@ -12,8 +12,8 @@ class Profile(models.Model):
     #linkedin = models.CharField('linkedin', max_length=100, null=True, blank=True)
     #facebook = models.CharField('facebook', max_length=100, null=True, blank=True)
     #instagram = models.CharField('instagram', max_length=100, null=True, blank=True)
-    topimage = models.imageField(upload_to='images', verbose_name='トップ画像')
-    subimage = models.imageField(upload_to='images', verbose_name='サブ画像')
+    topimage = models.ImageField(upload_to='images', verbose_name='トップ画像')
+    subimage = models.ImageField(upload_to='images', verbose_name='サブ画像')
     
     def __str__(self):
         return self.name
