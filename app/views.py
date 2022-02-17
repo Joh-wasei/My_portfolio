c8eeaa274b455f2a76f21d569a68e1448dd6c586
joh <j.odagiri.2011@gmail.com>
2022-02-17 11:30:28 +0900

    viewsファイルにIndex.html（トップページ）の設定を記述

--- app/views.py
+++ app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import View
+from .models import Profile
 # Create your views here.
+
+class IndexView(View):
+    def get(self, request, *args, **kwargs):
+        profile_data = Profile.object.all() #全てのprofileデータを取得
+        if profile_data.exists():
+            profile_data = profile_data.order_by('-id')[0] #降順に並べ替え、最新のprofileデータを取得
+        return render(request, 'app/index.html', {
+            'profile_data': profile_data
+        })
