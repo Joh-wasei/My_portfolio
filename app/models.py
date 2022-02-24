7f33b88359e31e8184e39391f2057125c8da2b82
joh <j.odagiri.2011@gmail.com>
2022-02-24 11:59:45 +0900

    職歴のモデルを作成

--- app/models.py
+++ app/models.py
@@ -29,3 +29,13 @@ class Work(models.Model):
     
     def __str__(self):
         return self.title
+    
+class Experience(models.Model):
+    occupation = models.CharField('職種', max_length=100)
+    company = models.CharField('会社', max_length=100)
+    description = models.TextField('説明')
+    place = models.CharField('場所', max_length=100)
+    period = models.CharField('期間', max_length=100)
+    
+    def __str__(self):
+        return self.occupation
