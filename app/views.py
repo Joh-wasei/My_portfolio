8f55fd2b2c2cde60b5552b40813bc2baea49bfb7
joh <j.odagiri.2011@gmail.com>
2022-03-01 11:16:06 +0900

    スキルをビューのアバウトクラスに追加

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import View
-from .models import Profile, Work, Experience, Education
+from .models import Profile, Work, Experience, Education, Software, Technical
 # Create your views here.
 
 class IndexView(View):
@@ -28,8 +28,12 @@ class AboutView(View):
             profile_data = profile_data.order_by('-id')[0]
         experience_data = Experience.objects.order_by('-id')
         education_data = Education.objects.order_by('-id')
+        software_data = Software.objects.order_by('-id')
+        technical_data = Technical.objects.order_by('-id')
         return render(request, 'app/about.html', {
             'profile_data': profile_data,
             'experience_data': experience_data,
             'education_data': education_data,
+            'software_data': software_data,
+            'technical_data': technical_data,
         })
